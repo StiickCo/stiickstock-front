aaab02ccb7eeb79cb3289f8f9829c460806d8129
Ryan <ryanvo1911@gmail.com>
2019-12-08 01:48:12 -0300

    Added vuetify and first basic product list page

---
+++ src/views/AddProducts.vue
@@ -0,0 +1,22 @@
+<template>
+  <form>
+    <v-text-field label="Nome do produto"/>
+    <v-text-field label="Descrição sobre o produto"/>
+    <v-text-field type="number" min="0" step=".01" label="Preço/cada"/>
+    <v-text-field type="number" min="0" label="Quantidade em estoque"/>
+    <v-btn @click="submit">Adicionar Produto</v-btn>
+  </form>
+</template>
+
+<script>
+export default {
+    name: "addProduct"
+}
+</script>
+
+<style scoped>
+
+form{
+    padding: 10px;
+}
+</style>
---
+++ src/views/ProductList.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Estoque
+      <v-spacer></v-spacer>
+      <v-text-field v-model="search" label="Procurar" single-line hide-details></v-text-field>
+    </v-card-title>
+    <v-data-table hide-default-footer :headers="headers" :items="products" :search="search">  
+
+        <template v-slot:item.productTotal="{ item }">
+            {{ item.productTotal = (item.productPrice * item.productQtd).toFixed(2) }}
+        </template>
+    </v-data-table>
+  </v-card>
+</template>
+<script>
+export default {
+    name: "listProduct",
+    data () {
+      return {
+        search: '',
+        headers: [{
+            text: 'ID',
+            align: 'left',
+            value: 'id',
+          },
+          { text: 'Nome do produto', value: 'productName'},
+          { text: 'Preço por item', value: 'productPrice' },
+          { text: 'Estoque atual', value: 'productQtd' },
+          { text: 'Valor total', value: 'productTotal' }
+        ],
+        products: [{
+            id: 1,
+            productName: 'Produto1',
+            productPrice: 50,
+            productQtd: 6,
+            productTotal: 0
+          },
+          {
+            id: 2,
+            productName: 'Produto2',
+            productPrice: 15.75,
+            productQtd: 9,
+            productTotal: 0
+          },
+          {
+            id: 3,
+            productName: 'Produto3',
+            productPrice: 6.99,
+            productQtd: 16,
+            productTotal: 0
+          },
+          {
+            id: 4,
+            productName: 'Produto4',
+            productPrice: 150,
+            productQtd: 3,
+            productTotal: 0
+          },
+          {
+            id: 5,
+            productName: 'Produto5',
+            productPrice: 20,
+            productQtd: 4,
+            productTotal: 0
+          },
+          {
+            id: 6,
+            productName: 'Produto6',
+            productPrice: 37.58,
+            productQtd: 11,
+            productTotal: 0
+          },
+          {
+            id: 7,
+            productName: 'Produto7',
+            productPrice: 22,
+            productQtd: 66,
+            productTotal: 0
+          },
+          {
+            id: 8,
+            productName: 'Produto8',
+            productPrice: 40,
+            productQtd: 32,
+            productTotal: 0
+          },
+          {
+            id: 9,
+            productName: 'Produto9',
+            productPrice: 25,
+            productQtd: 25,
+            productTotal: 0
+          },
+          {
+            id: 10,
+            productName: 'Produto10',
+            productPrice: 5,
+            productQtd: 150,
+            productTotal: 0
+          },
+        ],
+      }
+    },
+}
+</script>
+
+<style scoped>
+
+form{
+    padding: 10px;
+}
+</style>
