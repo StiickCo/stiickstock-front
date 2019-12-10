e6503605992a93f2d0f295c9a217817f18aca02a
BrunoCordioli <bcordiolimachado7@gmail.com>
2019-12-10 00:00:59 -0300

    Added methods for the list to use data from back-end

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -44,12 +44,12 @@
             
         </template>
 
-        <template v-slot:item.productTotal="{ item }">
-            {{ item.productTotal = (item.productPrice * item.productQtd).toFixed(2) }}
+        <template v-slot:item.total="{ item }">
+            {{ item.total = (item.price * item.quantity).toFixed(2) }}
         </template>
 
         <template v-slot:expanded-item="{ item }">
-            <td :colspan="headers.length" class="pa-4"> {{ item.productDescription}} </td>
+            <td :colspan="headers.length" class="pa-4"> {{ item.details}} </td>
         </template>
 
         <template v-slot:item.actions="{ item }">
@@ -100,9 +100,14 @@
   </v-card>
 </template>
 <script>
+import { APIService } from "@/resources/products.js";
+const api = new APIService();
 
 export default {
     name: "ProductList",
+    mounted () {
+      this.getProducts()
+    },
     data () {
       return {
         dialogEdit: false,
@@ -118,98 +123,18 @@ export default {
         prodEditDesc: '',
         search: '',
         headers: [{ text: 'ID', align: 'left', value: 'id'},
-          { text: 'Nome do produto', value: 'productName'},
-          { text: 'Preço por item', value: 'productPrice' },
-          { text: 'Estoque atual', value: 'productQtd' },
-          { text: 'Valor total', value: 'productTotal' },
+          { text: 'Nome do produto', value: 'name'},
+          { text: 'Preço por item', value: 'price' },
+          { text: 'Estoque atual', value: 'quantity' },
+          { text: 'Valor total', value: 'total' },
           { text: 'Ações', align: 'center', sortable: false, value: 'actions'},
         ],
-        products: [{
-            id: 1,
-            productName: 'Produto1',
-            productPrice: 50,
-            productQtd: 6,
-            productTotal: 0,
-            productDescription: "Descrição do produto 1"
-          },
-          {
-            id: 2,
-            productName: 'Produto2',
-            productPrice: 15.75,
-            productQtd: 9,
-            productTotal: 0,
-            productDescription: "Descrição do produto 2"
-          },
-          {
-            id: 3,
-            productName: 'Produto3',
-            productPrice: 6.99,
-            productQtd: 16,
-            productTotal: 0,
-            productDescription: "Descrição do produto 3"
-          },
-          {
-            id: 4,
-            productName: 'Produto4',
-            productPrice: 150,
-            productQtd: 3,
-            productTotal: 0,
-            productDescription: "Descrição do produto 4"
-          },
-          {
-            id: 5,
-            productName: 'Produto5',
-            productPrice: 20,
-            productQtd: 4,
-            productTotal: 0,
-            productDescription: "Descrição do produto 5"
-          },
-          {
-            id: 6,
-            productName: 'Produto6',
-            productPrice: 37.58,
-            productQtd: 11,
-            productTotal: 0,
-            productDescription: "Descrição do produto 6"
-          },
-          {
-            id: 7,
-            productName: 'Produto7',
-            productPrice: 22,
-            productQtd: 66,
-            productTotal: 0,
-            productDescription: "Descrição do produto 7"
-          },
-          {
-            id: 8,
-            productName: 'Produto8',
-            productPrice: 40,
-            productQtd: 32,
-            productTotal: 0,
-            productDescription: "Descrição do produto 8"
-          },
-          {
-            id: 9,
-            productName: 'Produto9',
-            productPrice: 25,
-            productQtd: 25,
-            productTotal: 0,
-            productDescription: "Descrição do produto 9"
-          },
-          {
-            id: 10,
-            productName: 'Produto10',
-            productPrice: 5,
-            productQtd: 150,
-            productTotal: 0,
-            productDescription: "Descrição do produto 10"
-          },
-        ],
+        products: []
       }
     },
     methods:{
         getDeleteProduct(item){
-            this.prodDeleteName = item.productName;
+            this.prodDeleteName = item.name;
             this.prodIndex = this.products.indexOf(item);
             this.dialogDelete = true;
         },
@@ -238,7 +163,7 @@ export default {
             let maxID = this.products.length+1;
             let newProd = {
                 id: maxID,
-                productName: this.prodEditName,
+                name: this.prodEditName,
                 productPrice: this.prodEditPrice,
                 productQtd: this.prodEditQtd,
                 productTotal: 0,
@@ -247,7 +172,11 @@ export default {
             this.products.push(newProd);
             this.dialogAdd = false;
         },
-
+        getProducts() {
+          let res = api.findAllProduct().then(data => {
+            this.products = data;
+          });
+        },
         clear(){
             this.prodEditName = '',
             this.prodEditPrice = '',
