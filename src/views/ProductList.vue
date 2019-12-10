d73f4d8c568737307a1e7b28e4fc57b198ed4963
NotGecko <ryanvo1911@gmail.com>
2019-12-10 00:44:30 +0000

    Added dialog boxes for edit, add and product delete

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -5,7 +5,38 @@
       <v-spacer></v-spacer>
       <v-text-field v-model="search" label="Procurar" single-line hide-details></v-text-field>
     </v-card-title>
-    <v-data-table hide-default-footer :headers="headers" :items="products" :search="search"  :single-expand=true show-expand>  
+    <v-data-table :headers="headers" :items="products" :search="search"  :single-expand=true show-expand>  
+
+    <template v-slot:top>
+        <div class="text-right mr-4">
+            <v-dialog v-model="dialogAdd" width="500">
+            <template v-slot:activator="{ on }">
+                <v-btn color="blue darken-3" dark v-on="on">Adicionar novo produto</v-btn>
+            </template>
+            <v-card>
+                <v-card-title class="headline grey lighten-2" primary-title>Adicionar novo produto</v-card-title>
+
+                <v-card-text>
+                <v-text-field required label="Nome do produto" v-model='prodEditName'></v-text-field>
+                <v-text-field type='number' step='0.01' label="Preço unitário" prefix="R$" v-model='prodEditPrice'></v-text-field>
+                <v-text-field type='number' label="Quantidade de produtos em estoque" v-model='prodEditQtd'></v-text-field>
+                <v-text-field label="Descrição do produto (opcional)" v-model='prodEditDesc'></v-text-field>
+                </v-card-text>
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-2" text @click="addProduct()">Adicionar produto</v-btn>
+                <v-btn color="red" text @click="dialogAdd = false">Cancelar</v-btn>
+                </v-card-actions>
+                
+                
+            </v-card>
+            </v-dialog>
+    </div>
+    
+    </template>
+
         <template v-slot:item.productQtd="{ item }">
             <v-btn icon> <v-icon >add</v-icon> </v-btn>
             <span> {{ item.productQtd }} </span>
@@ -23,18 +54,68 @@
 
         <template v-slot:item.actions="{ item }">
             <v-btn icon> <v-icon>info</v-icon> </v-btn>
-            <v-btn icon> <v-icon>edit</v-icon> </v-btn>
-            <v-btn icon> <v-icon>delete</v-icon> </v-btn>
+            <v-btn icon @click="getEditProdut(item)"> <v-icon>edit</v-icon> </v-btn>
+            <v-btn icon @click="getDeleteProduct(item)"> <v-icon>delete</v-icon> </v-btn>
         </template>
         <!-- Todo: dialog box when clicking edit/delete -->
     </v-data-table>
+
+    <!-- ********** DIALOGS ********** -->
+        <!-- Delete -->
+        <v-dialog v-model="dialogDelete" width="500">
+            <v-card>
+            <v-card-title class="headline grey lighten-2" primary-title>Deletar produto</v-card-title>
+    
+            <v-card-text class="subtitle-1">Deseja mesmo deletar o produto "{{ prodDeleteName }}"?</v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+                <v-btn color="red" text @click="removeProduct()">Deletar</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn color="blue-darken-2" text @click="dialogDelete = false">Cancelar</v-btn>
+            </v-card-actions>
+            </v-card>
+        </v-dialog>
+
+        <!-- Edit -->
+        <v-dialog v-model="dialogEdit" width="500">
+            <v-card>
+                <v-card-title class="headline grey lighten-2" primary-title>Editar produto</v-card-title>
+                <v-card-text>
+                <v-text-field required label="Nome do produto" v-model='prodEditName'></v-text-field>
+                <v-text-field type='number' step='0.01' label="Preço unitário" prefix="R$" v-model='prodEditPrice'></v-text-field>
+                <v-text-field type='number' label="Quantidade de produtos em estoque" v-model='prodEditQtd'></v-text-field>
+                <v-text-field label="Descrição do produto (opcional)" v-model='prodEditDesc'></v-text-field>
+                </v-card-text>
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-2" text @click="saveEdit()">Salvar</v-btn>
+                <v-btn color="red" text @click="dialogEdit = false">Cancelar</v-btn>
+                </v-card-actions>
+                
+                
+            </v-card>
+        </v-dialog>
   </v-card>
 </template>
 <script>
+
 export default {
     name: "ProductList",
     data () {
       return {
+        dialogEdit: false,
+        dialogDelete: false,
+        dialogAdd: false,
+        prodDeleteName: '',
+
+        prodIndex: 0,
+        
+        prodEditName: '',
+        prodEditPrice: 0,
+        prodEditQtd: 0,
+        prodEditDesc: '',
         search: '',
         headers: [{ text: 'ID', align: 'left', value: 'id'},
           { text: 'Nome do produto', value: 'productName'},
@@ -42,7 +123,6 @@ export default {
           { text: 'Estoque atual', value: 'productQtd' },
           { text: 'Valor total', value: 'productTotal' },
           { text: 'Ações', align: 'center', sortable: false, value: 'actions'},
-        //   { text: '', value: 'productDescription'}
         ],
         products: [{
             id: 1,
@@ -127,6 +207,47 @@ export default {
         ],
       }
     },
+    methods:{
+        getDeleteProduct(item){
+            this.prodDeleteName = item.productName;
+            this.prodIndex = this.products.indexOf(item);
+            this.dialogDelete = true;
+        },
+
+        removeProduct(){
+            this.products.splice(this.prodIndex, 1);
+            this.dialogDelete = false;
+        },
+
+        getEditProdut(item){
+            this.prodEditName = item.productName;
+            this.prodEditPrice = item.productPrice;
+            this.prodEditQtd = item.productQtd;
+            this.prodEditDesc = item.productDescription;
+            this.prodIndex = this.products.indexOf(item);
+            this.dialogEdit = true;
+        },
+        saveEdit(){
+            this.products[this.prodIndex].productName = this.prodEditName;
+            this.products[this.prodIndex].productPrice = this.prodEditPrice;
+            this.products[this.prodIndex].productQtd = this.prodEditQtd;
+            this.products[this.prodIndex].productDescription = this.prodEditDesc;
+            this.dialogEdit = false;
+        },
+        addProduct(){
+            let maxID = this.products.length+1;
+            let newProd = {
+                id: maxID,
+                productName: this.prodEditName,
+                productPrice: this.prodEditPrice,
+                productQtd: this.prodEditQtd,
+                productTotal: 0,
+                productDescription: this.prodEditDesc
+            }
+            this.products.push(newProd);
+            this.dialogAdd = false;
+        }
+    }
 }
 </script>
 
