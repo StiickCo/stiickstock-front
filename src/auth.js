f1961ef648fc425836ec28b41297f465987f7606
BrunoCordioli <bcordiolimachado7@gmail.com>
2019-12-16 21:02:29 -0300

    Modified package json and auth for the callback issue and a more smouth deployment

--- src/auth.js
+++ src/auth.js
@@ -3,8 +3,8 @@ import Vue from 'vue';
 import {store} from './store';
 
 
-let URL;
-if (location.href.indexOf('localhost:3000') > -1) {
+let URL = 'https://stiick-stock.surge.sh';
+if (location.href.indexOf('localhost') > -1) {
   URL = 'http://localhost:3000';
 }
 
@@ -13,7 +13,7 @@ const webAuth = new auth0.WebAuth({
   domain: 'stiick-co.auth0.com',
   clientID: 'AtspUzJO2z0LfbicomPUWxG0M8FXyc0h',
   // make sure this line is contains the port: 8080
-  redirectUri: `http://localhost:3000/callback`,
+  redirectUri: `${URL}/callback`,
   // we will use the api/v2/ to access the user information as payload
   audience: 'https://stiick-co.auth0.com/api/v2/',
   responseType: 'token id_token',
